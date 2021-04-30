da6656b72bea3d0ac8336bffa713a2dd94c1bcbb
RodolfoJaspe <macosi@Rodolfos-iMac.local>
2021-04-30 12:10:20 -0400

    changed styling

--- beer/src/components/Brewery.js
+++ beer/src/components/Brewery.js
@@ -5,7 +5,9 @@ function Brewery (props) {
     const {name, website_url, street, city, state, phone, zip} = props.brewery
     return (
         <div className="brewery">
-            <h2>{name}</h2>
+            <div className="breweryName">
+                <h2>{name}</h2>
+            </div>
             <iframe src={website_url} alt="website"></iframe>
             <div className="info">
                 <h5>{phone}</h5>
